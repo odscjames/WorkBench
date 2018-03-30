c488d629663508c6e3b5d38322c29bcca9e8fc9b
James Baster <james.baster@opendataservices.coop>
2018-03-30 12:53:18 +0100

    Add Database

--- vagrant/ocdsdata/bootstrap.sh
+++ vagrant/ocdsdata/bootstrap.sh
@@ -1,6 +1,16 @@
 
+set -e
+
+echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
+
+locale-gen
+
 apt-get update
-apt-get install -y python3 python3-pip
+apt-get install -y python3 python3-pip postgresql 
+
+sudo su --login -c "psql -c \"CREATE USER ocdsdata WITH PASSWORD 'ocdsdata';\"" postgres
+sudo su --login -c "psql -c \"CREATE DATABASE ocdsdata WITH OWNER ocdsdata ENCODING 'UTF8'  LC_COLLATE='en_GB.UTF-8' LC_CTYPE='en_GB.UTF-8'  TEMPLATE=template0 ;\"" postgres
+
 
 pip3 install virtualenv
  
