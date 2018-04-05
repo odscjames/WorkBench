02aafd7af228b6b013dfd4193bdd6ca088d0cd02
James Baster <james.baster@opendataservices.coop>
2018-04-05 11:34:32 +0100

    Postgres 10

--- vagrant/ocdsdata/bootstrap.sh
+++ vagrant/ocdsdata/bootstrap.sh
@@ -5,8 +5,12 @@ echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
 
 locale-gen
 
+echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
 apt-get update
-apt-get install -y python3 python3-pip postgresql 
+apt-get install -y python3 python3-pip postgresql-10 
 
 sudo su --login -c "psql -c \"CREATE USER ocdsdata WITH PASSWORD 'ocdsdata';\"" postgres
 sudo su --login -c "psql -c \"CREATE DATABASE ocdsdata WITH OWNER ocdsdata ENCODING 'UTF8'  LC_COLLATE='en_GB.UTF-8' LC_CTYPE='en_GB.UTF-8'  TEMPLATE=template0 ;\"" postgres
