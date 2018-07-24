00f6a74fe5844cd08a3092162b8d9297f317f3bb
James Baster <james.baster@opendataservices.coop>
2018-07-24 12:11:43 +0100

    Use Bionic for ocdsdata. add config file

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,11 @@ Vagrant.configure(2) do |config|
 
   config.vm.define "ocdsdata" do |normal|
 
+      config.vm.box = "ubuntu/bionic64"
+
+      config.vm.network "forwarded_port", guest: 9090, host: 9090
+      config.vm.network "forwarded_port", guest: 80, host: 8080
+
       config.vm.synced_folder "open-contracting-ocdsdata/", "/vagrant",  :owner=> 'ubuntu', :group=>'users', :mount_options => ['dmode=777', 'fmode=777']
 
       config.vm.provider "virtualbox" do |vb|
@@ -11,7 +16,7 @@ Vagrant.configure(2) do |config|
          vb.gui = false
 
         # Customize the amount of memory on the VM:
-        vb.memory = "1024"
+        vb.memory = "2048"
 
         # https://github.com/boxcutter/ubuntu/issues/82#issuecomment-260902424
         vb.customize [
--- vagrant/ocdsdata/bootstrap.sh
+++ vagrant/ocdsdata/bootstrap.sh
@@ -5,23 +5,26 @@ echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
 
 locale-gen
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
-
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-
 apt-get update
-apt-get install -y python3 python3-pip postgresql-10 
+apt-get install -y python3 python3-pip postgresql-10
 
 sudo su --login -c "psql -c \"CREATE USER ocdsdata WITH PASSWORD 'ocdsdata';\"" postgres
 sudo su --login -c "psql -c \"CREATE DATABASE ocdsdata WITH OWNER ocdsdata ENCODING 'UTF8'  LC_COLLATE='en_GB.UTF-8' LC_CTYPE='en_GB.UTF-8'  TEMPLATE=template0 ;\"" postgres
 
 
 pip3 install virtualenv
- 
+
 
 echo "alias db='psql -U  ocdsdata ocdsdata  -hlocalhost'" >> /home/vagrant/.bashrc
 echo "localhost:5432:ocdsdata:ocdsdata:ocdsdata" > /home/vagrant/.pgpass
 chown vagrant:vagrant /home/vagrant/.pgpass
 chmod 0600 /home/vagrant/.pgpass
 
+mkdir -p /home/vagrant/.config/ocdsdata
 
+echo "[DBHOST]" >> /home/vagrant/.config/ocdsdata/config.ini
+echo "HOSTNAME = localhost" >> /home/vagrant/.config/ocdsdata/config.ini
+echo "PORT = 5432" >> /home/vagrant/.config/ocdsdata/config.ini
+echo "USERNAME = ocdsdata" >> /home/vagrant/.config/ocdsdata/config.ini
+echo "PASSWORD = ocdsdata" >> /home/vagrant/.config/ocdsdata/config.ini
+echo "DBNAME = ocdsdata" >> /home/vagrant/.config/ocdsdata/config.ini
