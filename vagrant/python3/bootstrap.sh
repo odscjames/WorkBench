e2879dfe9dd7cf50c47f0b87a142bd6629757ef2
James Baster <james.baster@opendataservices.coop>
2018-10-25 15:06:25 +0100

    Add to Python 3

--- vagrant/python3/bootstrap.sh
+++ vagrant/python3/bootstrap.sh
@@ -8,4 +8,14 @@ apt-get install -y python3 python3-pip
 # flatten-tool needs this
 apt-get install libpython3.5-testsuite
 
+# translation needs this
+apt-get install gettext
+
+
 pip3 install flake8 pytest virtualenv
+
+
+# Install Google Clasp for App Script
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+npm i @google/clasp -g
