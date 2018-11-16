b052aca6e7fcbcb23524d7cd1d2223f43da451ec
James Baster <james.baster@opendataservices.coop>
2018-11-16 17:16:36 +0000

    Selenium

--- vagrant/python3/bootstrap.sh
+++ vagrant/python3/bootstrap.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt-get update
-apt-get install -y python3 python3-pip
+apt-get install -y python3 python3-pip unzip chromium-browser
 
 # flatten-tool needs this
 apt-get install libpython3.5-testsuite
@@ -19,3 +19,12 @@ pip3 install flake8 pytest virtualenv
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 npm i @google/clasp -g
+
+
+# Install Selenium stuff
+mkdir /home/vagrant/bin
+cd /tmp
+wget https://chromedriver.storage.googleapis.com/2.36/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /home/vagrant/bin
+
+export PATH=$PATH:`pwd`/chromedriver"
