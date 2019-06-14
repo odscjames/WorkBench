31970f7ebc41108be0790bb1d843386133fea442
James Baster <james.baster@opendataservices.coop>
2019-06-14 09:10:48 +0100

    Need to update salt!

--- vagrant/saltrunner/bootstrap.sh
+++ vagrant/saltrunner/bootstrap.sh
@@ -1,6 +1,9 @@
 
 set -e
 
+wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/2019.2 bionic main" >> /etc/apt/sources.list.d/saltstack.list
+
 apt-get update
 apt-get install -y salt-ssh python-minimal
 
