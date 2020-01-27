28dd5a846b5e93e038c4480a316e44097d02e528
James Baster <james.baster@opendataservices.coop>
2020-01-27 10:49:35 +0000

    SALTRUNNER to pip so you get Python 3 verson

--- vagrant/saltrunner/bootstrap.sh
+++ vagrant/saltrunner/bootstrap.sh
@@ -8,13 +8,19 @@ pip3 install salt-ssh
 
 cp /home/vagrant/parent-ssh/id_rsa /home/vagrant/.ssh/
 chmod 0600 /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
 
 mkdir -p /var/log/salt/
 chown vagrant:vagrant /var/log/salt/
+
+mkdir -p /var/cache/salt/
+chown vagrant:vagrant /var/cache/salt/
+
 mkdir -p /etc/salt/
 chown vagrant:vagrant /etc/salt/
 
 mkdir -p /home/vagrant/pki-dir
 chown vagrant:vagrant /home/vagrant/pki-dir
+
 mkdir -p /home/vagrant/cache-dir
 chown vagrant:vagrant /home/vagrant/cache-dir
