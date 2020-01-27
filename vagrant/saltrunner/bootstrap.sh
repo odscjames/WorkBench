46e9d9c9a7a8d279b8ebd8509e2adc8cc210b3a7
James Baster <james.baster@opendataservices.coop>
2020-01-27 10:23:09 +0000

    SALTRUNNER to pip so you get Python 3 verson

--- vagrant/saltrunner/bootstrap.sh
+++ vagrant/saltrunner/bootstrap.sh
@@ -1,19 +1,20 @@
 
 set -e
 
-wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-echo "deb http://repo.saltstack.com/apt/ubuntu/18.04/amd64/2019.2 bionic main" >> /etc/apt/sources.list.d/saltstack.list
-
 apt-get update
-apt-get install -y salt-ssh python3-minimal
+apt-get install -y python3 python3-pip
+
+pip3 install salt-ssh
 
 cp /home/vagrant/parent-ssh/id_rsa /home/vagrant/.ssh/
 chmod 0600 /home/vagrant/.ssh/id_rsa
 
+mkdir -p /var/log/salt/
 chown vagrant:vagrant /var/log/salt/
+mkdir -p /etc/salt/
 chown vagrant:vagrant /etc/salt/
 
-mkdir /home/vagrant/pki-dir
+mkdir -p /home/vagrant/pki-dir
 chown vagrant:vagrant /home/vagrant/pki-dir
-mkdir /home/vagrant/cache-dir
+mkdir -p /home/vagrant/cache-dir
 chown vagrant:vagrant /home/vagrant/cache-dir
